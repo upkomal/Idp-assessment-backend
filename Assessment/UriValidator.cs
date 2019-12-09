6d0dbb5d52dc6b0fa3baca1238f7d05b4a74b6ad
Komal Sharma <upadhyay.komal@gmail.com>
2019-12-09 13:13:26 +0100

    Create method to check and Validate URI

--- Assessment/UriValidator.cs
+++ Assessment/UriValidator.cs
@@ -1,12 +1,93 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace Assessment
 {
     public class UriValidator
     {
-        public Task<bool> IsUriValid(string requestedUri)
+        /// <summary>
+        ///Enum to define type of URI can send by User:
+        /// </summary>
+        public enum UriValidatorRules
         {
-            return Task.FromResult(false);
+            Wildcard,
+            Absolute,
+            Regex
         }
+
     }
+
+
+    /// <summary>
+    /// This Method accept string URI to validate in the set of validation rules in IEnumrable string:As a result it send a bool.
+    /// </summary>
+    public class UriValidators
+    {
+        public static bool Validate(string uri, IEnumerable<string> rules)
+        {
+            if (string.IsNullOrEmpty(uri)) return false;
+            bool result = false;
+            foreach (var rule in rules)
+            {
+                if (rule.StartsWith("@"))
+                {                      //Check for RegEx and call Method:
+                    result = ValidateRegexUri(uri, rule);
+                }
+                else if (rule.Contains("*"))
+                {
+                    result = ValidateWildcardUri(uri, rule);     //Check for Wildcard and call Method:
+                }
+                else
+                {
+                    result = ValidateAbsoluteUri(uri, rule);     //Check for Absolute and call Method:
+                }
+                if (result) return true;
+            }
+            return result;
+        }
+   
+
+    /// <summary>
+    /// Methos to Match uri with pattern specified in rule:
+    /// </summary>
+    /// <param name="uri"></param>
+    /// <param name="rule"></param>
+    /// <returns>True/False</returns>
+    private static bool ValidateRegexUri(string uri, string rule)
+    {
+        string regex = rule.Substring(1, rule.Length - 1);
+        var result = new Regex(regex).Match(uri);
+        return result.Success;
+    }
+
+    /// <summary>
+    /// Method to validate wildcard URI :
+    /// </summary>
+    /// <param name="uri"></param>
+    /// <param name="rule"></param>
+    /// <returns>True/False</returns>
+
+    private static bool ValidateWildcardUri(string uri, string rule)
+    {
+        string regex = rule.Replace("/", "\\/").Replace("*", "[\\w]+");
+        return Regex.Match(uri, regex).Success;
+    }
+
+    /// <summary>
+    /// Method to check Absolute URI:
+    /// </summary>
+    /// <param name="uri"></param>
+    /// <param name="rule"></param>
+    /// <returns>True/False</returns>
+    private static bool ValidateAbsoluteUri(string uri, string rule)
+    {
+        return rule.ToLower().Equals(uri.ToLower());
+
+    }
+    
+    }
+
 }
